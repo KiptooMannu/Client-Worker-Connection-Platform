d42384c44976500f5605a5e013d0e9c3a0b59a4a
KiptooMannu <kkgg7241@gmail.com>
2026-05-16 17:10:10 +0300

    Configure frontend environments to point to Render backend

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://client-search-backend.onrender.com/api'
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api'
+  apiUrl: 'https://client-search-backend.onrender.com/api' // Live backend
+  // apiUrl: 'http://localhost:8080/api' // Local backend
 };
 
 
