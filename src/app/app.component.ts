baf58ed571cbdac5e8e2a2750afc7dd4e300e2c8
KiptooMannu <kkgg7241@gmail.com>
2026-05-25 13:39:06 +0300

    all changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DevNavigatorComponent } from './shared/components/dev-navigator';
 import { LoadingComponent } from './shared/components/loading';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DevNavigatorComponent, LoadingComponent],
+  imports: [RouterOutlet, LoadingComponent],
   template: `
     <router-outlet />
     <app-loading />
-    <app-dev-navigator />
   `
 })
 export class AppComponent {}
