a6789a9d0513f7a345ef9164d2ff687d1eaf74ac
KiptooMannu <kkgg7241@gmail.com>
2026-05-29 10:39:27 +0300

    all changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,20 +1,9 @@
   export const environment = {
     production: false,
     frontendUrl: 'http://localhost:4200',
-    backendUrl: 'http://localhost:8080',
+    backendUrl: 'https://client-search-backend.onrender.com',
     apiUrl: 'https://client-search-backend.onrender.com/api',
-
-    authUrl: 'http://localhost:8080/api/auth'
+    authUrl: 'https://client-search-backend.onrender.com/api/auth'
   };
-
-
-  //   export const environment = {
-  //   production: false,
-  //   frontendUrl: 'https://client-worker-connection-platform.vercel.app/',
-  //   backendUrl: 'https://client-search-backend.onrender.com/api',
-  //   apiUrl: 'https://client-search-backend.onrender.com/api',
-
-  //   authUrl: 'https://client-search-backend.onrender.com/api/auth'
-  // };
 
 
